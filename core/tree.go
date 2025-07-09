a46564b687b2e22bcdb8ee745de2ce22076169ae
ElWillyGod <manuelantognazza@gmail.com>
2025-07-09 13:36:10 -0300

    struct tree v1

--- core/tree.go
+++ core/tree.go
@@ -3,3 +3,19 @@ package core
 /*
 implementacion de B+ Tree para almacenar los archivos y sus rutas.
 */
+
+type archivo struct {
+	nombre       string // No es necesario
+	ruta         string
+	esDirectorio bool
+}
+
+type listaArchivos struct {
+	clave    string
+	archivos []archivo
+}
+
+type nodo struct {
+	hojas      []listaArchivos
+	siguientes *nodo
+}
